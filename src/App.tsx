f55c3eea060d9f949b464f9a7286a013ecd084da
sleepingftx <sleepingftx@gmail.com>
2020-02-13 14:13:02 +0500

    Работает webpack с ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//import React from 'react'
+import * as React from 'react';
+
+
+const App: React.FC = () => {
+  return (
+        <>
+                <div className="container">
+                  123
+                </div>
+        </>
+        );
+}
+
+export default App;
